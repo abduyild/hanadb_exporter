09c81a2d7ff8d1bc7937ba1c8fdf0ec6567d30c9
Abdullah Yildirim <yildabdu@gmail.com>
2022-05-16 16:34:56 +0200

    use new structure to push metrics

--- hanadb_exporter/main.py
+++ hanadb_exporter/main.py
@@ -8,6 +8,19 @@ SAP HANA database prometheus data exporter app
 :since: 2019-05-09
 """
 
+import imp
+from opentelemetry.exporter.prometheus_remote_write import (
+    PrometheusRemoteWriteMetricsExporter
+)
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry import metrics
+from hanadb_exporter import secrets_manager
+from hanadb_exporter import utils
+from hanadb_exporter import db_manager
+from hanadb_exporter import prometheus_exporter
+from hanadb_exporter import __version__
+from prometheus_client import start_http_server
+from prometheus_client.core import REGISTRY
 import sys
 import os
 import traceback
@@ -16,34 +29,22 @@ from logging.config import fileConfig
 import time
 import json
 import argparse
-from opentelemetry import metrics as metrics_api
+from prometheus_remote_write import (
+    PrometheusRemoteWrite,
+    ExportResult
+)
 
 # getting the name of the directory
 # where the this file is present.
 current = os.path.dirname(os.path.realpath(__file__))
-  
+
 # Getting the parent directory name
 # where the current directory is present.
 parent = os.path.dirname(current)
-  
-# adding the parent directory to 
+
+# adding the parent directory to
 # the sys.path.
 sys.path.append(parent)
-
-from prometheus_client.core import REGISTRY
-from prometheus_client import start_http_server
-
-from hanadb_exporter import __version__
-from hanadb_exporter import prometheus_exporter
-from hanadb_exporter import db_manager
-from hanadb_exporter import utils
-from hanadb_exporter import secrets_manager
-
-from opentelemetry import metrics
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.exporter.prometheus_remote_write import (
-    PrometheusRemoteWriteMetricsExporter
-)
 
 
 LOGGER = logging.getLogger(__name__)
@@ -56,6 +57,7 @@ METRICS_FILES = [
     '/etc/hanadb_exporter/metrics.json',
     '/usr/etc/hanadb_exporter/metrics.json'
 ]
+
 
 def parse_config(config_file):
     """
@@ -75,9 +77,13 @@ def parse_arguments():
         "-c", "--config", help="Path to hanadb_exporter configuration file")
     parser.add_argument(
         "-m", "--metrics", help="Path to hanadb_exporter metrics file")
+    parser.add_argument('-t', nargs=1, dest='api_token', action='store',
+                        help='Sysdig API Token to use Prometheus Remote Write.')
+    parser.add_argument('-u', nargs=1, dest='url', action='store',
+                        help='Prometheus Remote Write complete url:path')
     parser.add_argument(
         "-d", "--daemon", action="store_true",
-        help="Start the exporter as a systemd daemon. Only used when the the application "\
+        help="Start the exporter as a systemd daemon. Only used when the the application "
              "is managed by systemd")
     parser.add_argument(
         "--identifier", help="Identifier of the configuration file from /etc/hanadb_exporter")
@@ -87,7 +93,10 @@ def parse_arguments():
     parser.add_argument(
         "-V", "--version", action="store_true",
         help="Print the hanadb_exporter version")
-    args = parser.parse_args()
+    try:
+        args = parser.parse_args()
+    except BaseException as exception:
+        exit(1)
     return args
 
 
@@ -96,14 +105,17 @@ def setup_logging(config):
     Setup logging system
     """
     hana_config = config.get('hana')
-    sufix = 'hanadb_exporter_{}_{}'.format(hana_config.get('host'), hana_config.get('port', 30015))
-    log_file = config.get('logging').get('log_file', '/var/log/{}'.format(sufix))
+    sufix = 'hanadb_exporter_{}_{}'.format(
+        hana_config.get('host'), hana_config.get('port', 30015))
+    log_file = config.get('logging').get(
+        'log_file', '/var/log/{}'.format(sufix))
 
-    fileConfig(config.get('logging').get('config_file'), defaults={'logfilename': log_file})
+    fileConfig(config.get('logging').get('config_file'),
+               defaults={'logfilename': log_file})
 
     # The next method is used to recatch and raise all
     # exceptions to redirect them to the logging system
-    def handle_exception(*exc_info): # pragma: no cover
+    def handle_exception(*exc_info):  # pragma: no cover
         """
         Catch exceptions to log them
         """
@@ -126,6 +138,8 @@ def lookup_etc_folder(config_files_path):
         'configuration file does not exist in {}'.format(",".join(config_files_path)))
 
 # Start up the server to expose the metrics.
+
+
 def run():
     """
     Main execution
@@ -140,7 +154,8 @@ def run():
     elif args.identifier is not None:
         file_name = args.identifier + '.json'
         # determine if file is /etc or /usr/etc
-        config_file = lookup_etc_folder([dirname + file_name for dirname in CONFIG_FILES_DIR])
+        config_file = lookup_etc_folder(
+            [dirname + file_name for dirname in CONFIG_FILES_DIR])
         config = parse_config(config_file)
 
     else:
@@ -167,7 +182,8 @@ def run():
         if aws_secret_name:
             LOGGER.info(
                 'AWS secret name is going to be used to read the database username and password')
-            db_credentials = secrets_manager.get_db_credentials(aws_secret_name)
+            db_credentials = secrets_manager.get_db_credentials(
+                aws_secret_name)
             user = db_credentials["username"]
             password = db_credentials["password"]
 
@@ -181,39 +197,44 @@ def run():
             ssl=hana_config.get('ssl', False),
             ssl_validate_cert=hana_config.get('ssl_validate_cert', False))
     except KeyError as err:
-        raise KeyError('Configuration file {} is malformed: {} not found'.format(args.config, err))
+        raise KeyError(
+            'Configuration file {} is malformed: {} not found'.format(args.config, err))
 
     if args.daemon:
         utils.systemd_ready()
 
     connectors = dbs.get_connectors()
-    collector = prometheus_exporter.SapHanaCollectors(connectors=connectors, metrics_file=metrics)
+    collector = prometheus_exporter.SapHanaCollectors(
+        connectors=connectors, metrics_file=metrics)
     REGISTRY.register(collector)
     LOGGER.info('exporter successfully registered')
 
     LOGGER.info('starting to serve metrics')
 
-    
-    # Sets the global MeterProvider instance
-    metrics_api.set_meter_provider(MeterProvider())
+    exporter = PrometheusRemoteWrite(args.url[0], args.api_token[0])
+    headers = exporter.build_headers()
 
-    # The Meter is responsible for creating and recording metrics. Each meter has a unique name, which we set as the module's name here.
-    meter = metrics_api.get_meter(__name__)
-    bearer_token = os.environ['SYSDIG_TOKEN']
-    exporter = PrometheusRemoteWriteMetricsExporter(
-        endpoint="https://ingest.eu-gb.monitoring.cloud.ibm.com/prometheus/remote/write",
-        headers={
-            "X-Scope-Org-ID": "5",
-            "Authorization": "Bearer " + bearer_token,
-        },)  # add other params as needed
-    metrics_api.get_meter_provider().start_pipeline(meter, exporter, 30)
+    metric_name = __name__
+
+    your_value = 0
+    extra_labels = {"my_labelname": "example", "other_label": "example_value2"}
+
+    timeserie = exporter.create_timeseries(
+        name=metric_name, value=your_value, extra_labels=extra_labels)
+    metrics = exporter.build_message([timeserie])
+    if exporter.send_message(metrics, headers) == ExportResult.SUCCESS:
+        print("Metric: " + metric_name + " with labels: " + str(extra_labels))
+    else:
+        print("There was an error pushing the data")
+
     #start_http_server(config.get('exposition_port', 9668), config.get('listen_address', '0.0.0.0'))
     # while True:
     #    time.sleep(1)
 
     #start_http_server(config.get('exposition_port', 9668), config.get('listen_address', '0.0.0.0'))
-    #while True:
+    # while True:
     #    time.sleep(1)
 
-if __name__ == "__main__": # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
     run()
